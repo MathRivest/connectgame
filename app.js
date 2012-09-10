ef4fee95c4af564b7ec72a3ce77f85f98323cec9
arnaud.lassignardie <arnaud@twistimage.com>
2012-09-10 16:41:20 -0400

    port listener

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(3001);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 io.configure(function (){
